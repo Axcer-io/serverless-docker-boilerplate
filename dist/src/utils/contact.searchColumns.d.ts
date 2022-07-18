36420b3e4b6768be8e9e5d929c1ef6f1d9cc24c1
nalakadharmadasa <dharmadasa.nalaka@gmail.com>
2022-07-18 10:29:41 +0530

    changes

--- dist/src/utils/contact.searchColumns.d.ts
+++ dist/src/utils/contact.searchColumns.d.ts
@@ -1,18 +1,12 @@
 export declare enum SearchColumns {
-    certificateId = "contains",
-    serialNumber = "contains",
-    subjectName = "exact",
-    issuerName = "contains",
-    status = "contains",
-    issuedAt = "contains",
-    expiredAt = "contains"
+    first_name = "contains",
+    last_name = "contains",
+    email = "contains",
+    mobile = "contains"
 }
 export declare enum SortColumns {
-    certificateId = 1,
-    serialNumber = 1,
-    subjectName = 1,
-    issuerName = 1,
-    status = 1,
-    issuedAt = 1,
-    expiredAt = 1
+    first_name = 1,
+    last_name = 1,
+    email = 1,
+    mobile = 1
 }
--- dist/src/utils/contact.searchColumns.js
+++ dist/src/utils/contact.searchColumns.js
@@ -3,22 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.SortColumns = exports.SearchColumns = void 0;
 var SearchColumns;
 (function (SearchColumns) {
-    SearchColumns["certificateId"] = "contains";
-    SearchColumns["serialNumber"] = "contains";
-    SearchColumns["subjectName"] = "exact";
-    SearchColumns["issuerName"] = "contains";
-    SearchColumns["status"] = "contains";
-    SearchColumns["issuedAt"] = "contains";
-    SearchColumns["expiredAt"] = "contains";
+    SearchColumns["first_name"] = "contains";
+    SearchColumns["last_name"] = "contains";
+    SearchColumns["email"] = "contains";
+    SearchColumns["mobile"] = "contains";
 })(SearchColumns = exports.SearchColumns || (exports.SearchColumns = {}));
 var SortColumns;
 (function (SortColumns) {
-    SortColumns[SortColumns["certificateId"] = 1] = "certificateId";
-    SortColumns[SortColumns["serialNumber"] = 1] = "serialNumber";
-    SortColumns[SortColumns["subjectName"] = 1] = "subjectName";
-    SortColumns[SortColumns["issuerName"] = 1] = "issuerName";
-    SortColumns[SortColumns["status"] = 1] = "status";
-    SortColumns[SortColumns["issuedAt"] = 1] = "issuedAt";
-    SortColumns[SortColumns["expiredAt"] = 1] = "expiredAt";
+    SortColumns[SortColumns["first_name"] = 1] = "first_name";
+    SortColumns[SortColumns["last_name"] = 1] = "last_name";
+    SortColumns[SortColumns["email"] = 1] = "email";
+    SortColumns[SortColumns["mobile"] = 1] = "mobile";
 })(SortColumns = exports.SortColumns || (exports.SortColumns = {}));
 //# sourceMappingURL=contact.searchColumns.js.map
